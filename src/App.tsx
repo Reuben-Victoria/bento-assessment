b623483e0f4f0cd9d38a50657c2f3175f09e6eca
Reuben Victoria <reubenvickie20@gmail.com>
2024-05-25 15:40:21 +0100

    feat: create cart-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { BookCard, Navbar, Pagination } from "./components";
+import { Cart } from "./pages";
 import "./index.css";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      <BookCard/>
-      <Pagination
-        page={5}
-        limit={10}
-        setLimit={() => {}}
-        onPageChange={() => {}}
-      />
+      <Cart/>
     </>
   );
 }
